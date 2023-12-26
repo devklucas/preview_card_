aca439554a0b09753202837d5eedb644cba79530
devklucas <lucascardoso64@gmail.com>
2023-12-26 16:58:11 -0300

    feat: complete a desktop and e mobile version

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import art from './assets/illustration-article.svg'
+import profile from './assets/image-avatar.webp'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main>
+      <div className='contentbox'>
+      <img src={art} alt="illustration article" className='illustration'/>
+        <section className='tagButton'>Learning</section>
+        <span className='datapublished'>Published 21 Dec 2023</span>
+        <a href="/" className="link">HTML & CSS foundations</a>
+        <span className='description'>These languages are the blackbone of every defining structure, content, and presentation</span>
+        <div className='infoprofile'>
+          <img src={profile} alt="profile" />
+          <p>Greg Hooper</p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div class="attribution">
+      Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+      Coded by <a href="https://www.linkedin.com/in/devklucas/">Devklucas</a>.
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+     
+    </main>
   )
 }
 
 export default App
+/*
+
+*/
